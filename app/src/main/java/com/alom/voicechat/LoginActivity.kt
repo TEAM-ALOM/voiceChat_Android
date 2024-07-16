46fa0e545e79b7c731e1d8ea1747962c2e744c7b
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-07-16 17:20:41 +0900

    Login Activity 추가 및 MainActivity 연결

---
+++ app/src/main/java/com/alom/voicechat/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.alom.voicechat
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 로그인 성공 시 MainActivity로 이동
+        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
