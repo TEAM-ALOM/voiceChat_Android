9a5d59c8fb44a05aa4fc7ab55a9f4da9c4662c86
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-07-22 03:02:55 +0900

    google login - signInWithGoogle 구현

--- app/src/main/java/com/alom/voicechat/LoginActivity.kt
+++ app/src/main/java/com/alom/voicechat/LoginActivity.kt
@@ -1,28 +1,78 @@
 package com.alom.voicechat
 
+
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.lifecycle.lifecycleScope
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import kotlinx.coroutines.launch
+import java.security.MessageDigest
+import java.util.UUID
+
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        // 로그인 성공 시 MainActivity로 이동
-        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        val googleSignInButton: Button = findViewById(R.id.google_sign_in_button)
+        googleSignInButton.setOnClickListener {
+            signInWithGoogle()
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val context = this
+        val credentialManager = CredentialManager.create(context)
+        val rawNonce = UUID.randomUUID().toString()
+        val bytes = rawNonce.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+        val hashedNonce = digest.fold("") { str, it-> str + "%02x".format(it) }
+
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId("475065480952-1gr9ks9cpichvlo887vjum44ipvv032l.apps.googleusercontent.com")
+            .setNonce(hashedNonce)
+            .build()
+
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+        lifecycleScope.launch {
+            try {
+                val result = credentialManager.getCredential(context,request)
+                val credential = result.credential
+
+                val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+                val googleIdToken = googleIdTokenCredential.idToken
+
+                Log.i(TAG, "Google ID Token: $googleIdToken")
+                Toast.makeText(context, "You are signed in!", Toast.LENGTH_SHORT).show()
+
+                // Main Activity로 이동
+                startActivity(Intent(context, MainActivity()::class.java))
+                finishAffinity()
+
+            } catch (e: GetCredentialException) {
+                Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                Log.i(TAG, e.message.toString())
+            } catch (e: GoogleIdTokenParsingException) {
+                Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                Log.i(TAG, e.message.toString())
+            }
+        }
     }
 }
