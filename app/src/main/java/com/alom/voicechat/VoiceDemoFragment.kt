f9be3394368fe0e9ed2858650b366569b6f7d877
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-08-05 23:38:59 +0900

    녹음 테스트 용도 VoiceDemo Fragment 추가

---
+++ app/src/main/java/com/alom/voicechat/VoiceDemoFragment.kt
@@ -0,0 +1,19 @@
+package com.alom.voicechat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alom.voicechat.databinding.FragmentSocialBinding
+
+class VoiceDemoFragment : Fragment() {
+    private lateinit var binding: FragmentSocialBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSocialBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
